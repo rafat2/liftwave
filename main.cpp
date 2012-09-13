c066643b2051f963b7611bb74749ec1ffb5fa3b8
Rafat Hussain <rafat.hsn@gmail.com>
2012-09-14 04:10:08 +0530

    commit Integer WT

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main()
 {
 
-string name="db3";
+string name="bior4.4";
 int J=2;
 double lp1_a[] = {0.9501,0.2311,0.6068,0.4860,0.8913,0.7621,0.4565,0.0185,0.8214,
 0.4447,0.6154,0.7919,0.9218,0.7382,0.1763,0.4057};
@@ -21,14 +21,7 @@ vector<double> sig;
     sig.assign(lp1_a,lp1_a + sizeof(lp1_a)/sizeof(double));
 
 liftscheme blift(name);
-string c="d";
-vector<double> addl;
-addl.push_back(0.500);
-addl.push_back(-0.125);
 
-int mp=0;
-blift.addLift(c,addl,mp);
-blift.disp();
 
 lwt<double> dlift(sig,blift,J);
 vector<double> a,d;
